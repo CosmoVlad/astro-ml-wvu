314ec448091d49fc5bf7b9bd442f0b2df93e4751
cosmoVlad <vnstrokov@gmail.com>
2024-09-11 23:42:22 -0400

    standard imports

---
+++ Preamble.py
@@ -0,0 +1,58 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# ### Standard imports and settings
+
+# %%
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+
+MEDIUM_SIZE = 18
+BIGGER_SIZE = 22
+
+plt.rcdefaults()
+
+#plt.rc('font',**{'family':'serif','serif':['Times']})
+#plt.rc('text', usetex=True)
+
+plt.rc('font', size=BIGGER_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+
+# %matplotlib inline
+
+# %% [markdown]
+# ### Settings for pretty plots
+
+# %%
+fig,ax = plt.subplots(ncols=1, nrows=1, figsize=(6,4))
+
+ax.grid(True,linestyle=':',linewidth='1.')
+ax.xaxis.set_ticks_position('both')
+ax.yaxis.set_ticks_position('both')
+ax.tick_params('both',length=3,width=0.5,which='both',direction = 'in',pad=10)
+
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$')
+
+#fig.tight_layout()
+#fig.savefig('test.pdf')
