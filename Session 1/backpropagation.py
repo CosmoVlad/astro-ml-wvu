beeab7661dbe6a3d2e0b31dd0d0dab2cad0e6363
cosmoVlad <vnstrokov@gmail.com>
2024-09-20 12:49:01 -0400

    post session 1

--- Session 1/backpropagation.py
+++ Session 1/backpropagation.py
@@ -13,7 +13,18 @@
 #     name: python3
 # ---
 
-# %%
+# %% [markdown]
+# Here we implement backpropagation for a simple network which predicts the range of motion for a projectile launched at an angle $\alpha$ with an initial velocity $v_0$.
+#
+# - refactor the forward propagation as two functions:
+#   - function `forward_prop` takes the input parameter vector $\mathbf{p}$ and network parameters $\left(W_1,W_2,\mathbf{b}_1,\mathbf{b}_2\right)$ (a.k.a. weights) and returns a prediction for $d$;
+#   - function `error_func` takes the prediction for $d$ and the weights $\left(W_1,W_2,\mathbf{b}_1,\mathbf{b}_2\right)$ and returns the mean squared error (MSE) between the prediction and the ground truth.
+# - use the JAX function `grad` to calculate the gradient of `error_func` w.r.t. the weights,
+# - use the GD method to minimize the MSE between predictions and the ground truth (a.k.a. training the NN).
+#
+# <ins>Follow-up</ins>: The GD below does not seem to result in optimal weights. Debug the pipeline.
+
+# %% jupyter={"source_hidden": true}
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
@@ -64,7 +75,7 @@ plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 # %%
 rng = np.random.default_rng()
 
-size = 100
+size = 1000
 g = 9.8
 
 velocities = 10*rng.uniform(size=size)        # v in [0,10]
@@ -74,11 +85,15 @@ distances = velocities**2/g * np.sin(2*angles)
 
 distances += rng.normal(loc=0., scale=distances/10, size=size)
 
-
 # %% [markdown]
 # #### Forward propagation
 
 # %%
+import jax.numpy as jnp
+from jax import grad, random
+
+from jax import config; config.update("jax_enable_x64", True)
+
 def layer_mapping(p_old, W, b):
 
     # p_old [m x N]
@@ -87,36 +102,35 @@ def layer_mapping(p_old, W, b):
     # W [n x m]
     # W * p_old + b
 
-    return np.dot(W, p_old) + b   # = p_new
+    return jnp.dot(W, p_old) + b   # = p_new
 
 def ReLU(p):
 
     return p * (p>0).astype(float)
 
+def sigmoid(p):
+
+    return 1 / (1 + jnp.exp(-p))
+
 # input [2x1] -> hidden1 [5x1] -> output [1x1]
 
-W1 = rng.normal(size=(5,2))
-W2 = rng.normal(size=(1,5))
-b1 = rng.normal(size=(5,1))
-b2 = rng.normal(size=(1,1))
 
+p_init = jnp.array([velocities,angles])
 
-#vel,alpha = velocities[0],angles[0]
-#p_init = np.array([[vel,alpha]]).T
+def forward_prop(p, W1, W2, b1, b2):
 
-p_init = np.array([velocities,angles])
+    p = layer_mapping(p, W1, b1)
+    p = ReLU(p)
+    p = layer_mapping(p, W2, b2)
+    p = ReLU(p)
 
-print(p_init.shape)
+    return p
 
-p = layer_mapping(p_init, W1, b1)
-p = ReLU(p)
-print(p.shape)
+def error_func(W1, W2, b1, b2, p_input, p_data):
 
-p = layer_mapping(p, W2, b2)
-p = ReLU(p)
-print(p.shape)
-
-err = np.sum((p - distances)**2) / size
+    p = forward_prop(p_input, W1, W2, b1, b2)
+    
+    return jnp.sum((p - p_data)**2) / size
 
 
 # %% [markdown]
@@ -130,3 +144,70 @@ err = np.sum((p - distances)**2) / size
 # W2 = W2 - step*grad_err(W2)
 # b1 = b1 - step*grad_err(b1)
 # b2 = b2 - step*grad_err(b2)
+
+# %%
+grad_err = grad(error_func, argnums=[0,1,2,3])
+
+step = 0.1
+n_steps = 2000
+
+seed = 1701
+
+key = random.key(seed)
+W1 = random.normal(key, shape=(5,2))
+
+key,_ = random.split(key)
+W2 = random.normal(key, shape=(1,5))
+
+key,_ = random.split(key)
+b1 = random.normal(key, shape=(5,1))
+
+key,_ = random.split(key)
+b2 = random.normal(key, shape=(1,1))
+
+error_values = [error_func(W1,W2,b1,b2,p_init,distances)]
+
+for i in range(n_steps):
+
+    grad_W1,grad_W2,grad_b1,grad_b2 = grad_err(W1,W2,b1,b2,p_init,distances)
+
+    W1 -= step * grad_W1
+    W2 -= step * grad_W2
+    b1 -= step * grad_b1
+    b2 -= step * grad_b2
+
+    error_values.append(error_func(W1,W2,b1,b2,p_init,distances))
+
+error_values = np.array(error_values)
+
+# %%
+fig,ax = plt.subplots(ncols=1, nrows=1, figsize=(6,4))
+
+ax.plot(np.arange(n_steps+1),error_values)
+
+ax.grid(True,linestyle=':',linewidth='1.')
+ax.xaxis.set_ticks_position('both')
+ax.yaxis.set_ticks_position('both')
+ax.tick_params('both',length=3,width=0.5,which='both',direction = 'in',pad=10)
+
+ax.set_xlabel('step')
+ax.set_ylabel('mean_sq_err')
+
+# %%
+key,_ = random.split(key)
+test_velocities = 10*random.uniform(key, shape=(size,))        # v in [0,10]
+
+key,_ = random.split(key)
+test_angles = jnp.pi/2 * random.uniform(key, shape=(size,))     # alpha in [0,pi/2]
+
+test_distances = test_velocities**2/g * jnp.sin(2*test_angles)
+test_distances += test_distances/10 * random.normal(key, shape=(size,))
+
+p_test = jnp.array([test_velocities,test_angles])
+
+pred_distances = forward_prop(p_test, W1,W2,b1,b2)
+
+# %%
+plt.scatter(test_distances, pred_distances.reshape(size,))
+
+# %%
--- Session 1/test_jax.py
+++ Session 1/test_jax.py
@@ -13,6 +13,19 @@
 #     name: python3
 # ---
 
+# %% [markdown]
+# In this notebook we test the `grad` function from JAX on 
+# $$
+# f(\mathbf{x})=\mathbf{x}^{\rm T}\mathbf{x}\,, \quad \mathbf{x}=\left(\begin{array}{c} x_1\\x_2\\ \vdots\\x_N\end{array}\right)
+# $$
+#
+# - implement $f(\mathbf{x})$ for an input of arbitrary length $N$,
+# - check that the `grad` applied to the function gives the right answer $\nabla_\mathbf{x} f = 2\mathbf{x}$,
+# - run the gradient descent (GD) algorithm for randomly initialized $\mathbf{x}_0\sim \mathcal{N}(0;1)$,
+# - check that it converges to the minimum $\mathbf{x}=\mathbf{0}$.
+#
+# <ins>Follow-up</ins>: Experiment with the step size and number of steps of the GD.
+
 # %%
 import numpy as np
 import scipy
@@ -57,6 +70,54 @@ from jax import grad
 
 from jax import config; config.update("jax_enable_x64", True)
 
+
 # %%
+def f(x):
+
+    return jnp.sum(x**2)
+
+grad_f = grad(f)
+
+# %%
+x = jnp.linspace(0,10,101)
+
+x[:10], grad_f(x)[:10]
+
+# %%
+rng = np.random.default_rng()
+
+x = rng.normal(size=101)
+
+print(x.shape, grad_f(x).shape)
+
+step = 0.9
+n_steps = 500
+
+x_values = [x]
+func_values = [f(x)]
+
+
+for i in range(n_steps):
+
+    x = x - step * grad_f(x)
+
+    x_values.append(x)
+    func_values.append(f(x))
+
+x_values = np.array(x_values)
+func_values = np.array(func_values)
+
+# %%
+fig,ax = plt.subplots(ncols=1, nrows=1, figsize=(6,4))
+
+ax.plot(np.arange(n_steps+1),func_values)
+
+ax.grid(True,linestyle=':',linewidth='1.')
+ax.xaxis.set_ticks_position('both')
+ax.yaxis.set_ticks_position('both')
+ax.tick_params('both',length=3,width=0.5,which='both',direction = 'in',pad=10)
+
+ax.set_xlabel('step')
+ax.set_ylabel('$f(x)$')
 
 # %%
