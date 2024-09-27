f59827d1b312d4145b85bd4cf9a7a5a848f2f1d3
cosmoVlad <vnstrokov@gmail.com>
2024-09-26 21:38:39 -0400

    post Session 2

--- Session 2/test_tf.py
+++ Session 2/test_tf.py
@@ -67,7 +67,7 @@ plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 # %%
 rng = np.random.default_rng()
 
-size = 1000
+size = 10000
 g = 9.8
 vrange = 10.
 alpha = np.pi/4
@@ -95,9 +95,104 @@ ax.set_xlabel('$v_0$')
 ax.set_ylabel('$d$')
 
 # %%
-import os
-os.environ['TFF_CPP_MIN_LOG_LEVEL'] = '2'
+# import os
+# os.environ['TFF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+# %%
+train_size = 10000
+
+velocities_train = 10*rng.uniform(size=train_size)        # v in [0,10]
+angles_train = np.pi/2 * rng.uniform(size=train_size)     # alpha in [0,pi/2]
+
+distances_train = velocities_train**2/g * np.sin(2*angles_train)
+
+distances_train += rng.normal(loc=0., scale=distances_train/10, size=train_size)
+
+x_train = np.c_[
+    velocities_train,angles_train
+]
+
+y_train = distances_train.reshape(train_size,-1)
+
+#x_train.shape, type(x_train)
+#y_train.shape
+
+# %%
+
+# architecture
+model = keras.Sequential(
+    [
+        layers.Dense(50, activation='relu'),
+        layers.Dense(100, activation='relu'),
+        layers.Dense(1, activation='relu'),
+    ]
+)
+
+# optimization
+model.compile(optimizer='Adam', loss='MSE')
+
+# training
+history = model.fit(
+    x_train, y_train, 
+    batch_size=64, epochs=100, validation_split=0.2, verbose=2
+)
+
+
+
+
+# %%
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.plot(loss)
+plt.plot(val_loss)
+
+# %%
+velocities_predict = np.linspace(0,10,100)        # v in [0,10]
+angles_predict = np.full_like(velocities_predict, np.pi/6)    # alpha in [0,pi/2]
+
+distances_truth = velocities_predict**2/g * np.sin(2*angles_predict)
+
+#distances_train += rng.normal(loc=0., scale=distances_train/10, size=train_size)
+
+x_predict = np.c_[
+    velocities_predict,angles_predict
+]
+
+distances_predict = model.predict(x_predict)
+
+# %%
+plt.plot(velocities_predict, distances_predict)
+plt.plot(velocities_predict, distances_truth)
+
+# %%
+test_size = 10000
+
+velocities_predict = vrange*rng.uniform(size=test_size)        # v in [0,10]
+angles_predict = np.pi/2 * rng.uniform(size=test_size)    # alpha in [0,pi/2]
+
+distances_truth = velocities_predict**2/g * np.sin(2*angles_predict)
+
+#distances_train += rng.normal(loc=0., scale=distances_train/10, size=train_size)
+
+x_predict = np.c_[
+    velocities_predict,angles_predict
+]
+
+distances_predict = model.predict(x_predict)
+
+# %%
+plt.scatter(distances_predict[:,0], distances_truth, s=5)
+
+# %%
+plt.hist(distances_predict[:,0] - distances_truth, bins=20)
+
+# %%
+distances_truth.shape
 
 # %%
