07ef10a300705ce67dd024b971405aeaea3a1f4a
cosmoVlad <vnstrokov@gmail.com>
2024-10-17 15:34:41 -0400

    post sess 5

--- Session 5/orbital_dynamics.py
+++ Session 5/orbital_dynamics.py
@@ -55,4 +55,201 @@ plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 #fig.savefig('test.pdf')
 
 # %% [markdown]
+# ## Paper summary
+#
+# ### Flowchart
+#
 # ![image](./arXiv210212695/Flowchart2.png)
+#
+# ### Equations
+#
+# #### Eqs. (5)
+# \begin{eqnarray}
+# 	\dot \phi
+# 	&=&
+# 	\frac{(1+e\cos(\chi))^2}{Mp^{3/2}}
+# 	\left(
+# 		1 + \mathcal{F}_1(\cos(\chi),p,e)
+# 	\right),\\
+# 	\dot \chi
+# 	&=&
+# 	\frac{(1+e\cos(\chi))^2}{Mp^{3/2}}
+# 	\left(
+# 		1 + \mathcal{F}_2(\cos(\chi),p,e)
+# 	\right),\\
+# 	\dot p
+# 	&=&
+# 	\mathcal{F}_3(p,e), \\
+# 	\dot e
+# 	&=&
+# 	\mathcal{F}_4(p,e),
+# \end{eqnarray}
+# with $\mathbf{x}(0) = (\phi_0,\chi_0,p_0,e_0)$.
+#
+# #### Eqs. (7)
+# \begin{eqnarray}
+# 	\mathbf{r}_1(t)
+# 	&=&
+# 	\frac{r(t) m_2}{M}\left(\cos(\phi(t)),\sin(\phi(t)), 0 \right) \,,
+# 	\\	
+#     \mathbf{r}_2(t)
+# 	&=&
+# 	-\frac{r(t) m_1}{M}\left(\cos(\phi(t)),\sin(\phi(t)), 0 \right) 
+# 	\,.
+# \end{eqnarray}
+#
+# #### Eqs. (8)
+# $$
+# h^{22}(t) =\dfrac{1}{r}\sqrt{\dfrac{4\pi}{5}}\left(\ddot{\mathcal{I}}_{xx}
+# - 2 i\ddot{\mathcal{I}}_{xy}-\ddot{\mathcal{I}}_{yy}\right)\,
+# $$
+#
+# #### Eqs. (A10) and (A11)
+# $$
+# {\mathcal{I}}^{ab} 
+# = I^{ab} - \frac{1}{3} \delta^{ab}\delta_{cd}I^{cd} \,,
+# $$
+# $\delta^{ab}$ is the Kronecker delta. 
+#
+# \begin{eqnarray}
+# 	I^{xx} & =& \int d^3x \rho x^2 = m_1 x_1(t)^2 + m_2 x_2(t)^2  \,, \\	
+#     I^{yy} & =& \int d^3x \rho y^2 = m_1 y_1(t)^2 + m_2 y_2(t)^2 \,, \\
+# 	I^{xy} & =& \int d^3x \rho xy = m_1 x_1(t)y_1(t) + m_2 x_2(t)y_2(t) \,,
+# \end{eqnarray}
+# and by symmetry $I^{xy} = I^{yx}$.
+#
+# #### Eq. (9)
+# \begin{equation}
+# 	\mathcal{J}(\mathbf{x})
+# 	=
+# 	\langle J(\mathbf{x},\cdot) \rangle
+# 						=
+# 	\frac{1}{T}
+# 	\int_{0}^{T}
+# 			J(\mathbf{x},t)
+# 	\,
+# 	\mathrm{d}t
+# 	,
+# \end{equation}
+# where $J(\mathbf{x},t) = \sum_{k} \big( w_k - w(t) \big)^2\delta(t-t_k)$
+# and bracket notation, $\langle \cdot \rangle$, denotes 
+# denotes averaging over the time interval.
+
+# %% [markdown]
+# ### Newtonian trajectories
+
+# %%
+from scipy.integrate import odeint
+
+def Newton_rhs(y,t):     # t -> t/M:   G=c=1
+                        # M -> GM/c^3
+
+    phi,chi,p,e = y
+
+    phi_dot = (1 + e*np.cos(chi))**2 / p**1.5
+    chi_dot = (1 + e*np.cos(chi))**2 / p**1.5
+    p_dot = 0.
+    e_dot = 0.
+
+    return np.array([phi_dot,chi_dot,p_dot,e_dot])
+
+def trajectory(phi,chi,p,e):
+
+    r = p/(1 + e*np.cos(chi))
+
+    return np.array([r*np.cos(phi),r*np.sin(phi)])
+
+
+# %%
+tinit = 0.
+tfin = 2*np.pi
+
+times = np.linspace(tinit, tfin, 100)
+
+y0 = np.array(
+    [0.,0.,1.,0.2]
+)
+
+sol = odeint(Newton_rhs, y0, times)
+sol.shape
+
+# %%
+x,y = trajectory(*sol.T)
+
+fig,ax = plt.subplots(ncols=1, nrows=1, figsize=(6,4))
+
+ax.plot(x,y)
+
+ax.grid(True,linestyle=':',linewidth='1.')
+ax.xaxis.set_ticks_position('both')
+ax.yaxis.set_ticks_position('both')
+ax.tick_params('both',length=3,width=0.5,which='both',direction = 'in',pad=10)
+
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$')
+
+
+# %% [markdown]
+# ### GR trajectories ("test particle")
+
+# %%
+def GR_rhs(y,t):     # t -> t/M:   G=c=1
+                        # M -> GM/c^3
+
+    phi,chi,p,e = y
+
+    phi_dot = (1 + e*np.cos(chi))**2 / p**1.5 * (p - 2 - 2*e*np.cos(chi)) / np.sqrt((p-2)**2 - 4*e**2)
+    chi_dot = (1 + e*np.cos(chi))**2 / p**2 * (p - 2 - 2*e*np.cos(chi)) *\
+                np.sqrt( (p - 6 - 2*e*np.cos(chi)) / ((p-2)**2 - 4*e**2))
+    p_dot = 0.
+    e_dot = 0.
+
+    return np.array([phi_dot,chi_dot,p_dot,e_dot])
+
+def Jtensor(x2,y2,q):
+
+    Ixx = q*x2**2
+    Iyy = q*y2**2
+    Ixy = q*x2*y2
+
+    trace = Ixx + Iyy
+
+    return np.array(
+        [Ixx - trace/3, Ixy],
+        [Ixy, Iyy - trace/3]
+    )
+
+
+# %%
+p0 = 100.
+e0 = 0.5
+
+tinit = 0.
+tfin = 2*np.pi * 100 * np.sqrt(p0**3)
+#tfin = 100000
+
+times = np.linspace(tinit, tfin, 100000)
+
+y0 = np.array(
+    [0.,0.,p0,e0]
+)
+
+sol = odeint(GR_rhs, y0, times)
+
+x,y = trajectory(*sol.T)
+
+fig,ax = plt.subplots(ncols=1, nrows=1, figsize=(6,4))
+
+ax.plot(x,y)
+
+ax.grid(True,linestyle=':',linewidth='1.')
+ax.xaxis.set_ticks_position('both')
+ax.yaxis.set_ticks_position('both')
+ax.tick_params('both',length=3,width=0.5,which='both',direction = 'in',pad=10)
+
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$')
+
+ax.set_aspect('equal')
+
+# %%
