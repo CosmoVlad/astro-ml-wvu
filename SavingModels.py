bf64cc4bf4e7a6af6d37c227d5ce7969935768b0
cosmoVlad <vnstrokov@gmail.com>
2024-12-11 11:47:25 -0500

    saving a custom model

--- SavingModels.py
+++ SavingModels.py
@@ -70,6 +70,7 @@ model = keras.Sequential(
         layers.Conv2D(5, 3, padding='same', activation='relu'),
         layers.MaxPooling2D(),
         layers.Conv2D(5, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
         layers.Flatten(),
         layers.Dense(10),
     ]
@@ -86,7 +87,7 @@ model.compile(
 # training
 history = model.fit(
     x_train, y_train, 
-    batch_size=64, epochs=20, validation_split=0.2, verbose=2,
+    batch_size=64, epochs=10, validation_split=0.2, verbose=1,
 )
 
 filename = 'mnist_standard.keras'
@@ -96,6 +97,95 @@ model.save(filename)
 model_trained = tf.keras.models.load_model(filename)
 
 predictions = model_trained.predict(x_test)
+
+predictions = np.argmax(predictions, axis=1)
+
+confusion_matrix = tf.math.confusion_matrix(y_test, predictions)
+
+fig,ax = plt.subplots(ncols=1, nrows=1, figsize=(6,6))
+
+ax.imshow(confusion_matrix)
+
+print(confusion_matrix)
+
+# %%
+from tensorflow.keras import saving
+
+saving.get_custom_objects().clear()
+
+@saving.register_keras_serializable(name="CustomBlock")
+class CustomBlock(keras.Layer):
+    def __init__(self, units, **kwargs):
+        super(CustomBlock, self).__init__(**kwargs)
+        self.units = units
+        self.conv2d = layers.Conv2D(self.units, 3, padding='same', activation='relu')
+        self.maxpool = layers.MaxPooling2D()
+        
+    def get_config(self):
+        base_config = super().get_config()
+        config = {
+            "units": self.units,
+        }
+        return {**base_config, **config}
+
+    def call(self, input_tensor, training=False):
+
+        x = self.conv2d(input_tensor)
+        x = self.maxpool(x)
+        return x
+
+@saving.register_keras_serializable(name="CustomModel")
+class CustomModel(keras.Model):
+    def __init__(self, units, **kwargs):
+        super(CustomModel, self).__init__(**kwargs)
+        self.units = units
+        self.block1 = CustomBlock(units)
+        self.block2 = CustomBlock(units)
+        self.flatten = layers.Flatten()
+        self.dense = layers.Dense(10)
+
+    def get_config(self):
+        base_config = super().get_config()
+        config = {
+            "units": self.units,
+        }
+        return {**base_config, **config}
+
+    def call(self, input_tensor, training=False):
+        x = self.block1(input_tensor, training=training)
+        x = self.block2(x)
+        x = self.flatten(x)
+
+        return self.dense(x)
+        
+
+
+# %%
+custom_model = CustomModel(5)
+
+# optimization
+custom_model.compile(
+    optimizer=keras.optimizers.Adam(), 
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
+    metrics=['accuracy'],
+    
+)
+
+
+# training
+history = custom_model.fit(
+    x_train, y_train, 
+    batch_size=64, epochs=10, validation_split=0.2, verbose=1,
+)
+
+filename = "mnist_custom.keras"
+custom_model.save(filename)
+
+# %%
+# Now, we can simply load without worrying about our custom objects.
+reconstructed_model = keras.models.load_model(filename)
+
+predictions = reconstructed_model.predict(x_test)
 
 predictions = np.argmax(predictions, axis=1)
 
